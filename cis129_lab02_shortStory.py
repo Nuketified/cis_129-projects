cba41b8fd6b03d407aef40f2f549572474dcd30c
Mat Bakarich <mbakarich1@mail.pima.edu>
2024-09-16 12:45:26 -0700

    Update cis129_lab02_shortStory.py
    updated with a story about my cat.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+#Author: Mat Bakarich
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+Print(My cat loves to go outside)
+Print(He even has his own catio.)
+Print(He would spend all his time outside if he had it his way)
+Print(He can't be outside all the time or the local lizard population would not survive.)

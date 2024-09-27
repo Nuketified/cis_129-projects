00e4fdf57915430de88a6c74b2b41078e66ccc29
Mat Bakarich <mbakarich1@mail.pima.edu>
2024-09-27 12:03:47 -0700

    Update CIS129_Mat_Bakarich_Lab4.py

--- CIS129_Mat_Bakarich_Lab4.py
+++ CIS129_Mat_Bakarich_Lab4.py
@@ -32,11 +32,11 @@ def getIncrease(prompt):
 def calcStoreBonus(monthlySales):
         if monthlySales >= 110000:
             storeAmount = 6000
-        elif monthlySales >= 10000:
+        elif monthlySales >= 100000:
             storeAmount = 5000
-        elif monthlySales >= 9000:
+        elif monthlySales >= 90000:
             storeAmount = 4000
-        elif monthlySales >= 8000:
+        elif monthlySales >= 80000:
             storeAmount = 3000
         else:storeAmount = 0
         return storeAmount

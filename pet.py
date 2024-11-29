6a17f9d4dd2072f838d8f54a9621b24f6f001610
Mat Bakarich <mbakarich1@mail.pima.edu>
2024-11-29 11:23:07 -0700

    Add files via upload

--- pet.py
+++ pet.py
@@ -3,27 +3,33 @@
 # November 20, 2024
 
 
-# Module to define "Pet" Class
 
+"""This class is for creating and editing 'Pet' objects in python. 
+
+The pet object stores 3 values, a string for name, a string for pet type, and an intger for age.
+The setter functions modify the values for a pet object.
+The getter functions return the assigned values.
+"""
 
 
 # define Pet class.
 class Pet:
     
 # fields
-    ####Private String name __name__
     
-    
-    
+    #Private String name 
     __pet_name = ""
-    ####Private String type
+   
+    #Private String type
     __pet_type = ""
-    ####Private String age
+    
+    #Private String age
     __pet_age = 0
 
-    ###############
-    # constructor #
-    ###############
+
+##############################
+##        constructor       ##
+##############################
 
     def __init__(self,pet_name,pet_type,pet_age):
         self.__pet_name = pet_name
@@ -39,16 +45,15 @@ class Pet:
             raise ValueError("Pet age must be an integer.")  
     
 
-    ############
-    # mutators #
-    ############
+##############################
+##         mutators         ##
+##############################
 
 
-    # Public Module setName(string a)
-        
+# Public Module set_name(string)   
     def set_name(self, pet_name):
         # validate input is a string
-        if type(pet_name) != str  or not pet_name.isspace:
+        if type(pet_name) != str  or pet_name.isspace():
             raise ValueError("Pet name must be a string.")
         # change the pet name
         else:
@@ -57,11 +62,10 @@ class Pet:
         
     
 
-    # Public Module setType(string)
-        
+# Public Module set_type(string)    
     def set_type(self, pet_type):
         # validate input is a string    
-        if type(pet_type) != str or not pet_type.isspace:
+        if type(pet_type) != str or pet_type.isspace():
             raise ValueError("Pet type must be a string.")
         # change pet type    
         else: 
@@ -70,38 +74,38 @@ class Pet:
               
     
 
-    # Public Module setAge(integer)
-        
+# Public Module set_age(integer)       
     def set_age(self, pet_age):
+        
         # validate input is an integer
         if type(pet_age) != int:
             raise ValueError("Pet age must be an integer.")
         if pet_age < 1:
-            raise ValueError("Pet age must be greater than 0.")
+            raise ValueError("Pet age must be greater than 0.")     
         # change pet age
         else:
-            self.__pet_age = pet_age 
+            self.__pet_age = pet_age
+                 
 
             
     
 
+###############################
+##         accessors         ##
+###############################
 
-    #############
-    # accessors # 
-    #############
 
-
-    # Public Function String getName()
+    # Public Function String get_name()
     def get_name(self): 
         return self.__pet_name
    
 
-    # Public Function String getType()
+    # Public Function String get_type()
     def get_type(self):
         return self.__pet_type
     
     
-    # Public function getAge()
+    # Public function Integer get_age()
     def get_age(self):
         return self.__pet_age
     
